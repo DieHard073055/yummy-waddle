bc2433e5fb91f4ef93fb68944fd06acc35a337f1
pamdayne <pam.dayne@gmail.com>
2015-09-10 11:25:47 +0800

    Task 2

---
+++ lab1/cd_rates.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>Lab 1 Task 2</title>
+    <style>
+      body{
+        margin: 30px;
+        margin-bottom: 0;
+        font-family: "Trebuchet Ms",sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+  <h1>Forestville Credit</h1>
+  <h2>CD Rates</h2>
+  <?php
+    echo "<ul>
+      <li>4.35% (36-Month Term CD)</li>
+      <li>3.85% (12-Month Term CD)</li>
+      <li>2.65% (6-Month Term CD)</li>
+    </ul>
+    ";
+    $str1 = "$1,000 minimum deposit";
+    echo "<p>".$str1."</p>";
+  ?>
+
+  </body>
+</html>
